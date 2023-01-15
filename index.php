ac56890510739043814603ce0b511a7fa08bb548
NanthapreechaSupama <nanthprichas@gmail.com>
2023-01-15 14:54:35 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form action ="show.php" method="POST" > 
+
+    <select name="prefix">
+        <option value="">-เลือก-</option>
+        <option value="ดช.">-ดช.-</option>
+        <option value="ดญ.">-ดญ.-</option>
+        <option value="นาย">-นาย-</option>
+        <option value="นาง">-นาง-</option>
+        <option value="นางสาว">-นางสาว-</option> 
+            
+    </select>
+
+    what is your name:<input type="text" name="name" ><br>
+    what is your age:<input type="text" name="age" ><br>
+    what is your gender:<input type="text" name="gender" ><br>
+    what is your status:<input type="text" name="status" ><br>
+    <input type="submit" value="submit"> 
+    
+</form>
+
+
+</body>
+</html>
---
+++ show.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php 
+    $prefix = $_POST["prefix"];
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $gender = $_POST["gender"];
+    $status = $_POST["status"];
+ 
+    
+    echo "Name:".$prefix.$name."<br>";
+    echo "Age:".$age."<br>";
+    echo "Gender:".$gender."<br>";
+    echo "Status:".$status."<br>";
+
+    ?>
+
+</body>
+
+</html>
